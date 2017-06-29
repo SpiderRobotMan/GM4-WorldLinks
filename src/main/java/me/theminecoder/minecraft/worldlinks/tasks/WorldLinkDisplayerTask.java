46483517516e9fa976f1cd99722bbe269e1b4d41
theminecoder <buisness@theminecoder.me>
2017-06-29 22:21:51 +1000

    you know what sucks? being sick. like it really really sucks

--- src/main/java/me/theminecoder/minecraft/worldlinks/tasks/WorldLinkDisplayerTask.java
+++ src/main/java/me/theminecoder/minecraft/worldlinks/tasks/WorldLinkDisplayerTask.java
@@ -2,7 +2,11 @@ package me.theminecoder.minecraft.worldlinks.tasks;
 
 import me.theminecoder.minecraft.worldlinks.WorldLinks;
 import me.theminecoder.minecraft.worldlinks.objects.LinkPlayer;
+import me.theminecoder.minecraft.worldlinks.utils.ParticleUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 /**
  * @author theminecoder
@@ -19,10 +23,17 @@ public class WorldLinkDisplayerTask implements Runnable {
     public void run() {
         plugin.getPlayerManager().getOnlinePlayers().stream()
                 .filter(LinkPlayer::isViewingWorldLinks)
-                .forEach(player -> player.getUnlockedLinks().stream()
-                        .filter(link -> !link.getConditions().stream().anyMatch(linkCondition ->
-                                !linkCondition.getType().valid(Bukkit.getPlayer(player.getUuid()), player, linkCondition.getConfig())
-                        ))
-                        .forEach(link -> link.displayToPlayer(player)));
+                .forEach(player -> {
+                    player.getUnlockedLinks().stream()
+                            .filter(link -> !link.getConditions().stream().anyMatch(linkCondition ->
+                                    !linkCondition.getType().valid(Bukkit.getPlayer(player.getUuid()), player, linkCondition.getConfig())
+                            ))
+                            .forEach(link -> {
+                                Player bukkitPlayer = Bukkit.getPlayer(player.getUuid());
+                                Location location = bukkitPlayer.getEyeLocation().getBlock().getLocation().add(0.5, 0.5, 0.5);
+                                location.add(bukkitPlayer.getLocation().clone().getDirection().normalize().add(new Vector(1, 0, 1)));
+                                ParticleUtils.sendParticle(bukkitPlayer, "REDSTONE", location, 1, 1);
+                            });
+                });
     }
 }
