cb29fcbc07ec898b9b33f982ffff5d16b760e3a8
MatrixTunnel <MatrixTunnel@users.noreply.github.com>
2017-10-12 12:56:31 -0700

    Make checking for link more efficient

--- src/main/java/co/gm4/worldlink/modules/triggers/Advancement.java
+++ src/main/java/co/gm4/worldlink/modules/triggers/Advancement.java
@@ -19,17 +19,7 @@ public class Advancement extends Module implements Listener {
             if ((event.getAdvancement().getKey().getNamespace() + ":"+ event.getAdvancement().getKey().getKey()).equals(link.getUnlockAdvancementKey())) {
                 LinkPlayer linkPlayer = WorldLink.get().getPlayerManager().getLinkPlayer(event.getPlayer().getUniqueId());
 
-                //TODO Make this more clean (just needed it working)
-                boolean found = false;
-
-                for (LinkWorld lWorld : linkPlayer.getWorlds()) {
-                    if (lWorld.getName().equals(link.getName())) {
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) linkPlayer.getWorlds().add(new LinkWorld(link.getName()));
+                if (linkPlayer.getWorlds().stream().noneMatch(linkWorld -> linkWorld.getName().equals(link.getName()))) linkPlayer.getWorlds().add(new LinkWorld(link.getName()));
             }
         });
     }
